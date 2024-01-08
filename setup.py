87a9cad16105c1135b03bd1d87776ccf0a9d51ab
Harrish Karthi <karthiharrish05@gmail.com>
2024-01-08 16:51:40 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+hyphen_e_dot = "-e ."
+
+def get_requirements(file_path: str) -> list:
+    '''
+    This function will return the list of requirements.
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+
+    return requirements
+
+setup(
+    name='ML_PROJ',
+    version='0.0.1',
+    author='Harrish',
+    author_email='karthiharrish05@gmail.com',
+    description='Your project description here',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
